ce433d29618cf383fea20573533e43a776df61ef
NataliaBravaya <Natalia.bravaya@yandex.ru>
2025-04-13 16:07:59 +0300

    add project

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Проектная работа Mesto
 
+# ссылка на репозиторий
+https://github.com/NataliaBravaya/mesto-project-ff
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,26 @@
-// @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
-// @todo: DOM узлы
 
-// @todo: Функция создания карточки
+const placesList = document.querySelector('.places__list');
 
-// @todo: Функция удаления карточки
 
-// @todo: Вывести карточки на страницу
+function to_create(data, deleteCallback) {
+    const card = cardTemplate.querySelector(".card").cloneNode(true);
+    const image = card.querySelector(".card__image");
+    image.alt = data.name;
+    image.src = data.link;
+    card.querySelector(".card__title").textContent = data.name;
+    card.querySelector(".card__delete-button").addEventListener('click', deleteCallback);
+    return card;
+}
+
+function delete_card (event) {
+    event.target.closest('.card').remove();
+}
+
+
+initialCards.forEach(card => {
+    const element = to_create(card, delete_card);
+    placesList.append(element);
+})
+
